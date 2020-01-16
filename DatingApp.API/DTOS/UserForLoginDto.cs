1f61f07dad98570916062e547fae07d044bece1a
Casey Whiteside <CWhiteside@macleanpower.com>
2020-01-16 10:56:25 -0500

    Add authentication middleware.

---
+++ DatingApp.API/DTOS/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.API.DTOS
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ DatingApp.API/DTOS/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.DTOS
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
